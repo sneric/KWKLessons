ec254100d02ea4c9a988cc4253b07ed0b89dae2e
sneric <sneric.smith@gmail.com>
2019-06-24 21:48:31 -0700

    iteration 3 complete

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // iteration 2
+    let emojis = ["😃" : "Happy face", "😢" : "Sad face"]
+    
+    //iteration 3
+    let customMessages = [
+        "Happy face" : ["You have a great smile!", "Keep the good vibes goin!", "Your smile is contagious!"],
+        "Sad face" : ["It's gonna be ok!", "What has got you down?", "I'm here to help."]
+                ]
+        
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,11 +32,37 @@ class ViewController: UIViewController {
         // when a user clicks an emoji button
         
         //user clicks on emoji, then an alert pops up with a message related to the emoji.
+       
+        // iteration 2
+        let selectedEmotion = sender.titleLabel?.text
         
-        let alertController = UIAlertController(title: "OK", message: "It's gonna be alright!", preferredStyle: UIAlertController.Style.alert)
+        // iteration 3
         
+        let randomNumber = Int.random(in: 0 ..< 3)
+        
+        // iteration 3
+        let emojiMessage =
+        
+        // here I want to extract the word that goes with my emoji
+        // then find that word in the dictionary
+        // get it's value (array)
+        // and access the first element
+        
+        customMessages[emojis[selectedEmotion!]!]?[randomNumber]
+        
+        //iteration 3 - another way to create function for random number generator
+//
+//        let emotionArray = customMessages[emojis[selectedEmotion!]!]
+//        let random = Int.random(in: 0 ..< emotionArray!.count)
+//        let emojiMessage = emotionArray?[random]
+        
+        // iteration 1 & 2 & 3
+        let alertController = UIAlertController(title: "Your Emojional Message", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+      
+        // iteration 1
         alertController.addAction(UIAlertAction(title: "OK", style:UIAlertAction.Style.default, handler: nil))
-        
+       
+        // iteration 1
         present(alertController, animated: true, completion: nil)
     }
 }
